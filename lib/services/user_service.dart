2d83c58be4ff0b4d00bb9672c235579899ff9836
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-09-25 02:37:47 +0900

    Implement signIn logic

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -16,6 +16,17 @@ class UserService with ChangeNotifier {
         .map(_userFromFirebaseUser);
   }
 
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
   Future registerWithEmailAndPassword(String email, String password) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
