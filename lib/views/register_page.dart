901a407b3d55c72378ab4417f2b6c0e7df4e0f98
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-09-25 02:25:29 +0900

    Implement register logic

--- lib/views/register_page.dart
+++ lib/views/register_page.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_provider/services/user_service.dart';
 
-
 class RegisterPage extends StatefulWidget {
+  final Function toggleView;
+  RegisterPage({this.toggleView});
+
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
   final UserService _auth = UserService();
+  final _formKey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login page'),
-      ),
+      appBar: AppBar(title: const Text('Register page'), actions: <Widget>[
+        FlatButton.icon(
+            onPressed: () {
+              widget.toggleView();
+            },
+            icon: const Icon(Icons.person),
+            label: const Text('Sign in'))
+      ]),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+            key: _formKey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an E-mail' : null,
                   onChanged: (val) {
                     setState(() {
                       email = val;
@@ -35,6 +46,8 @@ class _RegisterPageState extends State<RegisterPage> {
                 SizedBox(height: 20.0),
                 TextFormField(
                   obscureText: true,
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter a Password 6+ chars long' : null,
                   onChanged: (val) {
                     setState(() {
                       password = val;
@@ -49,13 +62,26 @@ class _RegisterPageState extends State<RegisterPage> {
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () async {
-                    print(email);
-                    print(password);
+                    if (_formKey.currentState.validate()) {
+                      print(email);
+                      print(password);
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'please supply a valid email';
+                        });
+                      }
+                    }
                   },
+                ),
+                SizedBox(height: 12.0),
+                Text(
+                  error ?? '',
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
                 )
               ],
-            )
-        ),
+            )),
       ),
     );
   }
